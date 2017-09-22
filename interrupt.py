cdc211847bb642204844c8c7261da9dd8b05bfa5
benjaminhabbel <benjaminhabbel@gmail.com>
2017-09-22 14:53:43 +0200

    update to interrupt.py

--- interrupt.py
+++ interrupt.py
@@ -1,145 +1,74 @@
 #!/usr/bin/python
-from __future__ import print_function
 import RPi.GPIO as GPIO
 import time
-import queue # https://pymotw.com/2/Queue/
 import subprocess
 
 #GPIO Pins
-Taster1 = 37
-Taster2 = 35
-redPin = 33
-greenPin = 31
-bluePin = 29
+TASTER_1 = 37
+TASTER_2 = 35
+RED_PIN = 33
+GREEN_PIN = 31
+BLUE_PIN = 29
 
 # GPIO-Nummer als Pinreferenz waehlen
 GPIO.setmode(GPIO.BOARD)  
 
 # Taster als Input deklarieren
-GPIO.setup(Taster1, GPIO.IN)
-GPIO.setup(Taster2, GPIO.IN)
+GPIO.setup(TASTER_1, GPIO.IN)
+GPIO.setup(TASTER_2, GPIO.IN)
 
 # LED als Output deklarieren
-GPIO.setup(redPin, GPIO.OUT)
-GPIO.setup(greenPin, GPIO.OUT)
-GPIO.setup(bluePin, GPIO.OUT)
+GPIO.setup(RED_PIN, GPIO.OUT)
+GPIO.setup(GREEN_PIN, GPIO.OUT)
+GPIO.setup(BLUE_PIN, GPIO.OUT)
 
 # Dictionary definieren. http://www.tutorialspoint.com/python/python_dictionary.htm
-dictionary = {}
-dictionary['pause'] = False
+status = "idle"
 
-queue = queue.Queue()
+def white_off():
+    GPIO.output(BLUE_PIN, GPIO.LOW)
+    GPIO.output(RED_PIN, GPIO.LOW)
+    GPIO.output(GREEN_PIN, GPIO.LOW)
 
-# Script pausieren/blockieren/beschaeftigen
-def Pause():
-   while dictionary['pause'] == True:
-       time.sleep(1)
-
-# RGB-LED konfigurieren
-def turnOn(pin):
-    GPIO.output(pin, GPIO.HIGH)
-
-def turnOff(pin):
-    GPIO.output(pin, GPIO.LOW)
-
-def redOn():
-    turnOff(redPin)
-
-def redOff():
-    turnOff(redPin)
-
-#def greenOn():
-#    turnOff(greenPin)
-
-#def greenOff():
-#    turnOff(greenPin)
-
-def blueOn():
-    turnOff(bluePin)
-
-#def blueOff():
-#    turnOff(bluePin)
-
-def yellowOn():
-    turnOff(redPin)
-    turnOff(greenPin)
-
-#def yellowOff():
-#    turnOff(redPin)
-#    turnOff(greenPin)
-
-#def cyanOn():
-#    turnOff(greenPin)
-#    turnOff(bluePin)
-
-#def cyanOff():
-#    turnOff(greenPin)
-#    turnOff(bluePin)
-
-#def magentaOn():
-#    turnOff(redPin)
-#    turnOff(bluePin)
-
-#def magentaOff():
-#    turnOff(redPin)
-#    turnOff(bluePin)
-
-#def whiteOn():
-#    turnOff(redPin)
-#    turnOff(greenPin)
-#    turnOff(bluePin)
-
-def whiteOff():
-    turnOff(redPin)
-    turnOff(greenPin)
-    turnOff(bluePin)
-
-# ISR
-def InterruptIn(pin):
-    if pin == Taster1:
-        if dictionary['pause'] == True:
-            whiteOff()
-            redOn()
-            print("mount usb, start motion")
+# InternalStartGeschichte
+def start_recording(pin):
+    if status == "idle":
+        status = "switching"
+        white_off()
+        GPIO.output(RED_PIN, GPIO.HIGH)
+        print("mount usb, start motion")
+        try:
             subprocess.run(["sudo", "mount", "/media/usb-video"])
-            subprocess.run(["sudo", "service", "motion", "start"])
-            dictionary['pause'] = False
-            redOff()
-            blueOn()
-        else:
-            queue.put(pin)
+        except Exception as e:
+            print("Error on mounting device:")
+            print(str(e))
+        subprocess.run(["sudo", "service", "motion", "start"])
+        white_off()
+        GPIO.output(GREEN_PIN, GPIO.HIGH)
+        status = "recording"
 
-def InterruptOut(pin):
-    if pin == Taster2:
-        if dictionary['pause'] == True:
-            whiteOff()
-            redOn()
-            print("stop motion, unmount usb")
-            subprocess.run(["sudo", "service", "motion", "stop"])
-            subprocess.run(["sudo", "umount", "/media/usb-video"])
-            dictionary['pause'] = False
-            redOff()
-            yellowOn()
-        else:
-            queue.put(pin)
+
+def stop_recording(pin):
+    if status == "recording":
+        status = "switching"
+        white_off()
+        GPIO.output(RED_PIN, GPIO.HIGH)
+        print("stop motion, unmount usb")
+        subprocess.run(["sudo", "service", "motion", "stop"])
+        subprocess.run(["sudo", "umount", "/media/usb-video"])
+        white_off()
+        GPIO.output(BLUE_PIN, GPIO.HIGH)
+        status = "idle"
+
 
 try:
     # Interrupt Event hinzufuegen. Auf steigende Flanke reagieren und ISR "Interrupt" deklarieren sowie Pin entprellen
-    GPIO.add_event_detect(Taster1, GPIO.RISING, callback=InterruptIn, bouncetime=200)
-    GPIO.add_event_detect(Taster2, GPIO.RISING, callback=InterruptOut, bouncetime=200)
+    GPIO.add_event_detect(TASTER_1, GPIO.RISING, callback=start_recording, bouncetime=200)
+    GPIO.add_event_detect(TASTER_2, GPIO.RISING, callback=stop_recording, bouncetime=200)
     # keep script running
     while True:
         time.sleep(0.5)
-        if not queue.empty():
-            job = queue.get()
-            if job == Taster1:
-                print("RECORD!")
-                dictionary['pause'] = True
-                Pause()
-            if job == Taster2:
-                print("PAUSE!")
-                dictionary['pause'] = True
-                Pause()
+        
 except (KeyboardInterrupt, SystemExit):
    GPIO.cleanup()
    print("\nQuit\n")
