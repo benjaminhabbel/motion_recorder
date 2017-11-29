9b36baddb2b255a1da867a95176486d0573ce2f2
benjaminhabbel <benjaminhabbel@gmail.com>
2017-11-29 18:05:42 +0100

    new pig_recorder.py file, first test succesful

--- interrupt.py
+++ interrupt.py
@@ -3,38 +3,8 @@ import os
 import RPi.GPIO as GPIO
 import time
 import subprocess
-from ffmpegwrapper import FFmpeg, Input, Output, VideoFilter
 
-#+-----+-----+---------+------+---+---Pi 3---+---+------+---------+-----+-----+
-#| BCM | wPi |   Name  | Mode | V | Physical | V | Mode | Name    | wPi | BCM |
-#+-----+-----+---------+------+---+----++----+---+------+---------+-----+-----+
-#|     |     |    3.3v |      |   |  1 || 2  |   |      | 5v      |     |     |
-#|   2 |   8 |   SDA.1 |   IN | 1 |  3 || 4  |   |      | 5v      |     |     |
-#|   3 |   9 |   SCL.1 |   IN | 1 |  5 || 6  |   |      | 0v      |     |     |
-#|   4 |   7 | GPIO. 7 |   IN | 1 |  7 || 8  | 0 | IN   | TxD     | 15  | 14  |
-#|     |     |      0v |      |   |  9 || 10 | 1 | IN   | RxD     | 16  | 15  |
-#|  *R |   0 | GPIO. 0 |  OUT | 0 + 11 || 12 + 0 | IN   | GPIO. 1 | 1   |*T1  |
-#|  *G |   2 | GPIO. 2 |  OUT | 0 + 13 || 14 +   |      | 0v      |     |*GND |
-#|  *B |   3 | GPIO. 3 |  OUT | 0 + 15 || 16 + 0 | IN   | GPIO. 4 | 4   |*T2  |
-#|*3,3V|     |    3.3v |      |   + 17 || 18 | 0 | IN   | GPIO. 5 | 5   | 24  |
-#|  10 |  12 |    MOSI |   IN | 0 | 19 || 20 |   |      | 0v      |     |     |
-#|   9 |  13 |    MISO |   IN | 0 | 21 || 22 | 0 | IN   | GPIO. 6 | 6   | 25  |
-#|  11 |  14 |    SCLK |   IN | 0 | 23 || 24 | 1 | IN   | CE0     | 10  | 8   |
-#|     |     |      0v |      |   | 25 || 26 | 1 | IN   | CE1     | 11  | 7   |
-#|   0 |  30 |   SDA.0 |   IN | 1 | 27 || 28 | 1 | IN   | SCL.0   | 31  | 1   |
-#|   5 |  21 | GPIO.21 |  OUT | 1 | 29 || 30 |   |      | 0v      |     |     |
-#|   6 |  22 | GPIO.22 |  OUT | 0 | 31 || 32 | 0 | IN   | GPIO.26 | 26  | 12  |
-#|  13 |  23 | GPIO.23 |  OUT | 0 | 33 || 34 |   |      | 0v      |     |     |
-#|  19 |  24 | GPIO.24 |   IN | 1 | 35 || 36 | 0 | IN   | GPIO.27 | 27  | 16  |
-#|  26 |  25 | GPIO.25 |   IN | 1 | 37 || 38 | 0 | IN   | GPIO.28 | 28  | 20  |
-#|     |     |      0v |      |   | 39 || 40 | 0 | IN   | GPIO.29 | 29  | 21  |
-#+-----+-----+---------+------+---+----++----+---+------+---------+-----+-----+
-#| BCM | wPi |   Name  | Mode | V | Physical | V | Mode | Name    | wPi | BCM |
-#+-----+-----+---------+------+---+---Pi 3---+---+------+---------+-----+-----+
-
-# sudo apt install pip
-# pip install ffmpegwrapper
-#import ffmpegwrapper
+#sudo chown -R pi:pi /media/
 
 # GPIO pins
 TASTER_1 = 12
@@ -65,13 +35,11 @@ GPIO.output(BLUE_PIN, GPIO.HIGH)
 # LABEL="mp-video-2a" UUID="a5025313-3cef-407a-b320-531c64f49083"
 # LABEL="mp-video-2b" UUID="176d600d-646b-40d7-83ec-bb13641fb03e"
 
-
 # LED off
 def led_off():
     GPIO.output(BLUE_PIN, GPIO.LOW)
     GPIO.output(RED_PIN, GPIO.LOW)
     GPIO.output(GREEN_PIN, GPIO.LOW)
-
 
 # error LED
 def error():
@@ -92,11 +60,10 @@ def start_recording(pin):
         led_off()
         GPIO.output(RED_PIN, GPIO.HIGH)
         print("mount usb, start ffmpeg")
-        #subprocess.call(["sudo", "mkdir", "/media/usb-video"])
         if not os.path.exists('/media/usb-video'):
             os.mkdirs('/media/usb-video')
             return
-	subprocess.call([
+        subprocess.call([
             "sudo",
             "mount",
             "/dev/sda1",
@@ -107,18 +74,11 @@ def start_recording(pin):
             GPIO.output(BLUE_PIN, GPIO.HIGH)
             status = "idle"
             return
-
-        #subprocess.check_call(["sudo", "motion"])
         os.chdir('/media/usb-video')
-        #videofilter1 = VideoFilter().drawtext('fontsize=12', 'text="%(localtime\:%T)', 'fontcolor=white', 'x=1', 'y=1', 'rate=3')
-	#videofilter2 = VideoFilter().file('segment', 'segment_time=3600', 'strftime=1')
-	#input_video = Input('/dev/video0')
-	#output_video = Output('camera1-%Y%m%d-%H%M.avi', videofilter1, videofilter2)
-	#FFmpeg('ffmpeg', input_video, output_video)
-	#subprocess.Popen(["ffmpeg -i /dev/video0 -vf drawtext=fontsize=12: text='%{localtime\:%T}': fontcolor=white: x=1: y=1: rate=3 -r 3 -an -f segment -segment_time 3600 -strftime 1 camera1-%Y%m%d-%H%M.avi"])
-        cmd = "ffmpeg -i /dev/video0 -vf drawtext=fontsize=12: text='%{localtime\:%T}': fontcolor=white: x=1: y=1: rate=3 -r 3 -an -f segment -segment_time 3600 -strftime 1 camera1-%Y%m%d-%H%M.avi"
-	subprocess.call(cmd, shell=True)
-	led_off()
+        cmd = "ffmpeg -i /dev/video0 -r 3 -f segment -segment_time 3600 -strftime 1 camera1-%Y%m%d-%H%M.avi &"
+        #-vf drawtext (x=8 y=8 box=1 fontcolor=white boxcolor=black expansion=strftime text='$newline %Y-%m-%d %H\\:%M\\:%S')
+        subprocess.call(cmd, shell=False)
+        led_off()
         GPIO.output(GREEN_PIN, GPIO.HIGH)
         status = "recording"
 
@@ -163,4 +123,3 @@ if __name__ == "__main__":
     finally:
         GPIO.cleanup()
         print("\nQuit\n")
-
