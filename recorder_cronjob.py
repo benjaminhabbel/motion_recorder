2847d2c12cf970cefe074d169464008cdf626937
benjaminhabbel <benjaminhabbel@gmail.com>
2017-12-04 17:17:39 +0100

    bla bla

--- recorder_cronjob.py
+++ recorder_cronjob.py
@@ -3,6 +3,6 @@ import recordlib
 
 if __name__ == "__main__":
     if os.path.ismount('/media/usb-video'):
-    	recordlib.initialize()
+        recordlib.initialize()
         recordlib.stop_recording(0)
         recordlib.start_recording(0)
