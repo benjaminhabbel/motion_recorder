5e56e11424065d532e550ec13fd3b2a998d55e3e
schubisu <robin.schubert@ghi-muenster.de>
2017-12-06 21:11:58 +0100

    pig_recorder as class

--- pig_recorder.py
+++ pig_recorder.py
@@ -6,163 +6,237 @@ import subprocess
 import logging
 import shutil
 import sys
+import datetime
+from datetime import datetime
 
-# GPIO pins
-TASTER_1 = 12
-TASTER_2 = 16
-RED_PIN = 11
-GREEN_PIN = 13
-BLUE_PIN = 15
 
-# use GPIO board numbers
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
+class PigRecorder():
 
-# button = input
-GPIO.setup(TASTER_1, GPIO.IN)
-GPIO.setup(TASTER_2, GPIO.IN)
+    # USB-Sticks by UUID
+    # LABEL="mp-video-1a" UUID="699c84be-41e2-423c-819a-02c0dbdf9664"
+    # LABEL="mp-video-1b" UUID="a4724cb0-f5d6-424d-a34a-f28b3ece39a8"
+    # LABEL="mp-video-2a" UUID="a5025313-3cef-407a-b320-531c64f49083"
+    # LABEL="mp-video-2b" UUID="176d600d-646b-40d7-83ec-bb13641fb03e"
 
-# LED = output
-GPIO.setup(RED_PIN, GPIO.OUT)
-GPIO.setup(GREEN_PIN, GPIO.OUT)
-GPIO.setup(BLUE_PIN, GPIO.OUT)
+    # GPIO pins
+    TASTER_1 = 12
+    TASTER_2 = 16
+    RED_PIN = 11
+    GREEN_PIN = 13
+    BLUE_PIN = 15
 
-# define state
-status = "idle"
-GPIO.output(BLUE_PIN, GPIO.HIGH)
+    states = {
+        'idle': BLUE_PIN,
+        'recording': GREEN_PIN,
+        'switching': RED_PIN,
+    }
 
-# debug logging
-logSrc = '/home/pi/motion_recorder/pig_recorder.log'
-logDst = '/media/usb-video/pig_recorder.log'
+    # debug logging
+    log_src = '/home/pi/motion_recorder/pig_recorder.log'
+    log_dst = '/media/usb-video/pig_recorder.log'
+    video_path = '/media/usb-video'
+
+    def __init__(self):
+        # use GPIO board numbers
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
+
+        # button = input
+        GPIO.setup(self.TASTER_1, GPIO.IN)
+        GPIO.setup(self.TASTER_2, GPIO.IN)
+
+        # LED = output
+        GPIO.setup(self.RED_PIN, GPIO.OUT)
+        GPIO.setup(self.GREEN_PIN, GPIO.OUT)
+        GPIO.setup(self.BLUE_PIN, GPIO.OUT)
+
+        # define state
+        self.set_status("idle")
+
+        GPIO.add_event_detect(
+            self.TASTER_1,
+            GPIO.RISING,
+            callback=self.start_recording,
+            bouncetime=1000
+        )
+        GPIO.add_event_detect(
+            self.TASTER_2,
+            GPIO.RISING,
+            callback=self.stop_recording,
+            bouncetime=1000
+        )
+
+        stdout_logger = logging.getLogger('STDOUT')
+        sys.stdout = StreamToLogger(
+            stdout_logger,
+            self.log_src,
+            logging.INFO
+        )
+
+        stderr_logger = logging.getLogger('STDERR')
+        sys.stderr = StreamToLogger(
+            stderr_logger,
+            self.log_src,
+            logging.ERROR
+        )
+
+    def led_off(self):
+        '''
+        LED off
+        '''
+        GPIO.output(self.BLUE_PIN, GPIO.LOW)
+        GPIO.output(self.RED_PIN, GPIO.LOW)
+        GPIO.output(self.GREEN_PIN, GPIO.LOW)
+
+    def set_status(self, state):
+        self.status = state
+        self.led_off()
+        GPIO.output(
+            self.states[state],
+            GPIO.HIGH
+        )
+
+    def error(self):
+        '''
+        error LED
+        '''
+        self.led_off()
+        for i in range(10):
+            GPIO.output(self.RED_PIN, GPIO.HIGH)
+            time.sleep(0.05)
+            GPIO.output(self.RED_PIN, GPIO.LOW)
+            time.sleep(0.05)
+
+    def mount_drive(self):
+        if os.path.ismount(self.video_path):
+            return True
+
+        self.set_status('switching')
+        logging.info("mounting usb")
+
+        if not os.path.exists(self.video_path):
+            try:
+                os.mkdir(self.video_path)
+                subprocess.call([
+                    "sudo",
+                    "mount",
+                    "/dev/sda1",
+                    self.video_path
+                ])
+            except Exception as e:
+                logging.error(e)
+                self.error()
+                return False
+
+        return os.path.ismount(self.video_path)
+
+
+    def unmount_drive(self):
+        if not os.path.ismount(self.video_path):
+            return True
+
+        self.set_status("switching")
+        logging.info("unmounting usb")
+
+        shutil.copyfile(self.log_src, self.log_dst)
+        subprocess.call(["sudo", "sync"])
+        os.chdir("/home/pi")
+        try:
+            subprocess.check_call([
+                "sudo",
+                "umount",
+                self.video_path
+            ])
+        except Exception as e:
+            logging.error("could not unmount drive")
+            logging.error(e)
+            return False
+
+        try:
+            subprocess.call(["sudo", "rm", "-r", self.video_path])
+        except Exception as e:
+            logging.error("could not remove usb-video directory")
+            logging.error(e)
+            return False
+
+        logging.info('unmounting complete')
+        self.led_off()
+        GPIO.output(self.BLUE_PIN, GPIO.HIGH)
+        return True
+
+
+    def start_recording(self, pin):
+        '''
+        start record
+        '''
+        if self.status == "idle" and self.mount_drive():
+            rec_time = datetime.today()
+            os.chdir(self.video_path)
+            subprocess.Popen([
+                "ffmpeg", "-i", "/dev/video0",
+                "-vf", "drawtext=x=8: y=8: box=1: fontcolor=white: boxcolor=black: expansion=strftime: text='%T'",
+                "-r", "3",
+                "camera1-{}.avi".format(
+                   datetime.strftime(
+                      rec_time,
+                      "%Y%m%d-%H%M%S"
+                   )
+                )
+            ])
+            self.set_status('recording')
+
+    def stop_recording(self, pin):
+        '''
+        stop recording
+        '''
+        if self.status == "recording":
+            logging.info('killing ffmpeg')
+            subprocess.call(["sudo", "killall", "ffmpeg"])
+            self.unmount_drive()
+
+
+    def restart_recording(self):
+        if self.status == "recording":
+            logging.info('killing ffmpeg')
+            subprocess.call(["sudo", "killall", "ffmpeg"])
+            self.start_recording(0)
+
 
 class StreamToLogger(object):
-   """
-   Fake file-like stream object that redirects writes to a logger instance.
-   """
-   def __init__(self, logger, log_level=logging.INFO):
-      self.logger = logger
-      self.log_level = log_level
-      self.linebuf = ''
+    """
+    Fake file-like stream object that redirects
+    writes to a logger instance.
+    """
+    def __init__(self, logger, log_src, log_level=logging.INFO):
+        self.logger = logger
+        self.log_level = log_level
+        self.linebuf = ''
+        self.log_src = log_src
 
-   def write(self, buf):
-      for line in buf.rstrip().splitlines():
-         self.logger.log(self.log_level, line.rstrip())
+    def write(self, buf):
+        for line in buf.rstrip().splitlines():
+            self.logger.log(self.log_level, line.rstrip())
 
-logging.basicConfig(filename=logSrc,
-                format="%(asctime)s | %(name)s | %(levelname)s | %(message)s",
-                datefmt='%Y/%m/%d - %H:%M:%S',
-                level=logging.DEBUG)
-
-stdout_logger = logging.getLogger('STDOUT')
-sl = StreamToLogger(stdout_logger, logging.INFO)
-sys.stdout = sl
-
-stderr_logger = logging.getLogger('STDERR')
-sl = StreamToLogger(stderr_logger, logging.ERROR)
-sys.stderr = sl
-
-# USB-Sticks by UUID
-# LABEL="mp-video-1a" UUID="699c84be-41e2-423c-819a-02c0dbdf9664"
-# LABEL="mp-video-1b" UUID="a4724cb0-f5d6-424d-a34a-f28b3ece39a8"
-# LABEL="mp-video-2a" UUID="a5025313-3cef-407a-b320-531c64f49083"
-# LABEL="mp-video-2b" UUID="176d600d-646b-40d7-83ec-bb13641fb03e"
-
-# LED off
-def led_off():
-    GPIO.output(BLUE_PIN, GPIO.LOW)
-    GPIO.output(RED_PIN, GPIO.LOW)
-    GPIO.output(GREEN_PIN, GPIO.LOW)
-
-# error LED
-def error():
-    i = 0
-    led_off()
-    for i in range(10):
-        GPIO.output(RED_PIN, GPIO.HIGH)
-        time.sleep(0.05)
-        GPIO.output(RED_PIN, GPIO.LOW)
-        time.sleep(0.05)
-
-
-# start record
-def start_recording(pin):
-    global status
-    if status == "idle":
-        status = "switching"
-        led_off()
-        GPIO.output(RED_PIN, GPIO.HIGH)
-        print("mount usb, start ffmpeg")
-        logging.info("Button 1 -> mount usb, start ffmpeg")
-        if not os.path.exists('/media/usb-video'):
-            os.mkdir('/media/usb-video')
-        subprocess.call([
-            "sudo",
-            "mount",
-            "/dev/sda1",
-            "/media/usb-video"
-        ])
-        if not os.path.ismount('/media/usb-video'):
-            error()
-            GPIO.output(BLUE_PIN, GPIO.HIGH)
-            status = "idle"
-            return
-        os.chdir('/media/usb-video')
-        subprocess.Popen([
-            "ffmpeg", "-i", "/dev/video0",
-            "-vf", "drawtext=x=8: y=8: box=1: fontcolor=white: boxcolor=black: expansion=strftime: text='%T'",
-            "-r", "3",
-            "-f", "segment", "-segment_time", "3600", "-strftime", "1", "camera1-%Y%m%d-%H%M%S.avi"
-        ])
-        led_off()
-        GPIO.output(GREEN_PIN, GPIO.HIGH)
-        status = "recording"
-
-
-# stop record
-def stop_recording(pin):
-    global status
-    if status == "recording":
-        status = "switching"
-        led_off()
-        GPIO.output(RED_PIN, GPIO.HIGH)
-        print("stop ffmpeg, unmount usb")
-        logging.info("Button 2 -> stop ffmpeg, unmount usb")
-        shutil.copyfile(logSrc, logDst)
-        subprocess.call(["sudo", "killall", "ffmpeg"])
-        subprocess.call(["sudo", "sync"])
-        try:
-            os.chdir("/home/pi")
-            subprocess.check_call(["sudo", "umount", "/media/usb-video"])
-        except:
-            print('could not unmount drive')
-            logging.error("could not unmount drive")
-            status = "recording"
-            return
-        subprocess.call(["sudo", "rm", "-r", "/media/usb-video"])
-        led_off()
-        GPIO.output(BLUE_PIN, GPIO.HIGH)
-        status = "idle"
+        logging.basicConfig(
+           filename=self.log_src,
+           format="%(asctime)s | %(name)s | %(levelname)s | %(message)s",
+           datefmt='%Y/%m/%d-%H:%M:%S',
+           level=logging.DEBUG
+        )
 
 
 if __name__ == "__main__":
-    print("waiting for input")
+
+    recorder = PigRecorder()
+    start_time = datetime.now()
+
     logging.info("waiting for input")
     try:
-        # define interrupt, get rising signal, debounce pin
-        GPIO.add_event_detect(
-            TASTER_1,
-            GPIO.RISING,
-            callback=start_recording,
-            bouncetime=1000
-        )
-        GPIO.add_event_detect(
-            TASTER_2,
-            GPIO.RISING,
-            callback=stop_recording,
-            bouncetime=1000
-        )
-        # keep script running
         while True:
+            time_span = datetime.now() - start_time
+            if time_span.total_seconds() >= 3600:
+                start_time = datetime.now()
+                recorder.restart_recording()
             time.sleep(0.5)
 
     finally:
