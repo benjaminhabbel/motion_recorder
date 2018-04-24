1e068b232b1e169e3d3b3bfd34dfea4a3a62508a
Benjamin Habbel <benjaminhabbel@gmail.com>
2018-04-25 00:01:14 +0200

    removed old UUIDs

--- pig_recorder.py
+++ pig_recorder.py
@@ -11,12 +11,6 @@ from datetime import datetime
 
 class PigRecorder():
 
-    # USB-Sticks by UUID
-    # LABEL="mp-video-1a" UUID="699c84be-41e2-423c-819a-02c0dbdf9664"
-    # LABEL="mp-video-1b" UUID="a4724cb0-f5d6-424d-a34a-f28b3ece39a8"
-    # LABEL="mp-video-2a" UUID="a5025313-3cef-407a-b320-531c64f49083"
-    # LABEL="mp-video-2b" UUID="176d600d-646b-40d7-83ec-bb13641fb03e"
-
     # GPIO pins
     TASTER_1 = 12
     TASTER_2 = 16
